6514de00dd3067095da7df8b555b19c2da2c43cf
Naaman Newbold <naaman@heroku.com>
2012-01-22 14:54:41 -0800

    Attempt at redis session manager.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
 import com.naamannewbold.ondeck.auth.AuthFilter;
+import com.ovea.jetty.session.redis.RedisSessionIdManager;
+import com.ovea.jetty.session.redis.RedisSessionManager;
+import com.ovea.jetty.session.serializer.JsonSerializer;
 import com.sun.jersey.api.core.ResourceConfig;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.session.HashSessionManager;
@@ -6,6 +9,12 @@ import org.eclipse.jetty.server.session.SessionHandler;
 import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Protocol;
+
+import java.net.URI;
+import java.net.URL;
 
 /**
  * TODO: Javadoc
@@ -15,12 +24,28 @@ import org.eclipse.jetty.servlet.ServletHolder;
 public class Main {
     public static void main(String... args) throws Exception {
         int port = Integer.valueOf(System.getProperty("port", (System.getenv("PORT") != null) ? System.getenv("PORT") : "8080"));
+        URI redisConfigURL = new URI(System.getProperty("REDISTOGO_URL", System.getenv("REDISTOGO_URL")));
+
         Server server = new Server(port);
+
+        JedisPool jedisPool = new JedisPool(new JedisPoolConfig(),
+                redisConfigURL.getHost(),
+                redisConfigURL.getPort(),
+                Protocol.DEFAULT_TIMEOUT,
+                redisConfigURL.getUserInfo().split(":",2)[1]);
+
         ServletContextHandler context = new ServletContextHandler();
         context.setContextPath("/");
-        context.setSessionHandler(new SessionHandler(new HashSessionManager()));
+
+        RedisSessionManager sessionManager = new RedisSessionManager(jedisPool, new JsonSerializer());
+        sessionManager.setSessionPath("/");
+        context.setSessionHandler(new SessionHandler(sessionManager));
+
+        RedisSessionIdManager sessionIdManager = new RedisSessionIdManager(server, jedisPool);
+        server.setSessionIdManager(sessionIdManager);
+
         server.setHandler(context);
-        
+
         ServletHolder holder = new ServletHolder(com.sun.jersey.spi.container.servlet.ServletContainer.class);
         holder.setInitParameter("com.sun.jersey.config.property.packages", "com.naamannewbold.ondeck");
         holder.setInitParameter(ResourceConfig.PROPERTY_CONTAINER_REQUEST_FILTERS, AuthFilter.class.getName());
